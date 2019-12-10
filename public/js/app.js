3b6714c39cdf77b510819b598c8daa0538e3e80c
awatef <awatefalmenwer@gmail.com>
2019-12-10 15:40:41 +0300

    Remove unnecssary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,7 @@
-console.log('Client side javascript file is loaded!')
- 
-//  fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//      response.json().then((data) => {
-//          if(data.error) {
-//              console.log('error')
-//          } else {
-//              console.log(data.location)
-//              console.log(data.forecast)
-//              }
-//      })
-
-//  })
- const weatherForm = document.querySelector('form')
- const search = document.querySelector('input')
- const messageOne = document.querySelector('#message-1')
- const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
  
 
